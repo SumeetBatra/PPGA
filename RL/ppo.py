improvements to ppo code and fixes to obs/rew normalization

RL/ppo.py
@@ -199,16 +199,17 @@ class PPO:
                     )
                     v_loss_clipped = (v_clipped - b_returns[:, mb_inds].flatten()) ** 2
                     v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
-                    v_loss = 0.5 * v_loss_max.mean()
+                    v_loss = v_loss_max.mean()
                 else:
-                    v_loss = 0.5 * ((newvalue - b_returns[:, mb_inds].flatten()) ** 2).mean()
+                    v_loss = ((newvalue - b_returns[:, mb_inds].flatten()) ** 2).mean()
 
                 entropy_loss = entropy.mean()
                 loss = pg_loss - self.cfg.entropy_coef * entropy_loss + v_loss * self.cfg.vf_coef
 
-                self.vec_optimizer.zero_grad()
-                self.qd_critic_optim.zero_grad()
-                self.mean_critic_optim.zero_grad()
+                for (p1, p2, p3) in zip(self.vec_inference.parameters(), self.mean_critic.parameters(), self.qd_critic.parameters()):
+                    p1.grad = None
+                    p2.grad = None
+                    p3.grad = None
                 loss.backward()
                 nn.utils.clip_grad_norm_(self.vec_inference.parameters(), self.cfg.max_grad_norm)
                 self.vec_optimizer.step()
@@ -442,7 +443,10 @@ class PPO:
             axis=1)
         avg_traj_lengths = traj_lengths.to(torch.float32).reshape((vec_agent.num_models, vec_env.num_envs // vec_agent.num_models)).\
             mean(dim=1).cpu().numpy()
-        metadata = np.array([{'traj_length': t} for t in avg_traj_lengths]).reshape(-1,)
+        metadata = np.array([{'traj_length': t,
+                              'mean_critic_params': self.mean_critic.serialize(),
+                              'qd_critic_params': self.qd_critic.serialize()}
+                             for t in avg_traj_lengths]).reshape(-1,)
         max_reward = np.max(total_reward)
         min_reward = np.min(total_reward)
         mean_reward = np.mean(total_reward)

utils/normalize_obs.py
@@ -13,16 +13,14 @@ class RunningMeanStd(nn.Module):
         # TODO: these should be float64. Fix this
         self.register_buffer('mean', torch.zeros(shape, dtype=torch.float32))
         self.register_buffer('var', torch.ones(shape, dtype=torch.float32))
-        self.count = epsilon
+        self.register_buffer('count', torch.ones([1], dtype=torch.float32))
 
     def update(self, x):
         """ update from a batch of samples"""
         batch_mean = torch.mean(x, dim=0)
         batch_var = torch.var(x, dim=0)
         batch_count = x.shape[0]
-        self.mean, self.var, self.count = self.update_from_moments(batch_mean, batch_var, batch_count)
-        self.get_buffer('mean')[:] = self.mean
-        self.get_buffer('var')[:] = self.var
+        self.mean[:], self.var[:], self.count[:] = self.update_from_moments(batch_mean, batch_var, batch_count)
 
     def update_from_moments(self, batch_mean, batch_var, batch_count):
         batch_mean = batch_mean.to(self.mean.device)
@@ -58,7 +56,8 @@ class NormalizeObservation(nn.Module):
         self.epsilon = epsilon
 
     def forward(self, obs):
-        obs = self.normalize(obs)
+        with torch.no_grad():
+            obs = self.normalize(obs)
         return obs
 
     def normalize(self, obs):
@@ -79,15 +78,16 @@ class NormalizeReward(nn.Module):
     def __init__(self, num_envs, reward_dim=1, gamma: float = 0.99, epsilon: float = 1e-8):
         super(NormalizeReward, self).__init__()
         self.num_envs = num_envs
-        self.return_rms = RunningMeanStd(shape=())
+        self.return_rms = RunningMeanStd(shape=(reward_dim,))
         self.returns = torch.zeros((self.num_envs, reward_dim))
         self.gamma = gamma
         self.epsilon = epsilon
 
     def forward(self, rews, dones):
-        self.returns = self.returns * self.gamma + rews.reshape(self.returns.shape)
-        rews = self.normalize(rews)
-        self.returns[dones.long()] = 0.0
+        with torch.no_grad():
+            self.returns = self.returns * self.gamma + rews.reshape(self.returns.shape)
+            rews = self.normalize(rews)
+            self.returns[dones.long()] = 0.0
         return rews
 
     def normalize(self, rews):