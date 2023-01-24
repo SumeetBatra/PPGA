a5cbdf9d750dcdfa5faf8cf370837f6b7bebc022
Sumeet Batra <sscifi@gmail.com>
2023-01-23 18:26:37 -0800

    update slurm runners

--- runners/slurm/train_walker2d_slurm.sh
+++ runners/slurm/train_walker2d_slurm.sh
@@ -4,31 +4,44 @@
 #SBATCH -n1
 #SBATCH -c4
 #SBATCH --output=tmp/method3_walker2d_%j.log
+# the following params need to be set manually
+# PROJECT_PATH: path to QDax root on your system
+# ENV_NAME: which environment to run on (see qdax/environments/__init__.py for a list of all runnable envs)
+# then from the project root dir you can run ./scripts/train_pga_me.sh
 
-srun python -m algorithm.train_qdppo \
-            --env_name=walker2d \
-            --algorithm=qd-ppo \
-            --seed=0000 \
-            --rollout_length=128 \
-            --use_wandb=True \
-            --wandb_group=QDPPO \
-            --num_dims=2 \
-            --num_minibatches=4 \
-            --update_epochs=2 \
-            --normalize_obs=False \
-            --normalize_rewards=True \
-            --wandb_run_name=qdppo_walker2d_12_13_22 \
-            --mega_lambda=200 \
-            --env_type=brax \
-            --env_batch_size=3000 \
-            --learning_rate=0.001 \
-            --vf_coef=2 \
-            --max_grad_norm=1 \
-            --dqd_lr=0.01 \
-            --pretrain=False \
-            --total_iterations=1000 \
-            --dqd_algorithm=cma_maega \
-            --sigma0=1.0 \
-            --restart_rule=no_improvement
-            --torch_deterministic=False \
-            --logdir=./logs/qdppo_walker2d_12_13_22 \
+ENV_NAME="walker2d"
+GRID_SIZE=50  # number of cells per archive dimension
+SEED=1111
+
+
+RUN_NAME="paper_ppga_"$ENV_NAME"_seed_"$SEED"_adaptive_stddev"
+echo $RUN_NAME
+srun python -m algorithm.train_qdppo --env_name=$ENV_NAME \
+                                --rollout_length=128 \
+                                --use_wandb=True \
+                                --wandb_group=paper \
+                                --num_dims=2 \
+                                --seed=$SEED \
+                                --anneal_lr=False \
+                                --num_minibatches=8 \
+                                --update_epochs=4 \
+                                --normalize_obs=True \
+                                --normalize_rewards=True \
+                                --wandb_run_name=$RUN_NAME\
+                                --popsize=300 \
+                                --env_batch_size=3000 \
+                                --learning_rate=0.001 \
+                                --vf_coef=2 \
+                                --entropy_coef=0.0 \
+                                --target_kl=0.008 \
+                                --max_grad_norm=1 \
+                                --total_iterations=2000 \
+                                --dqd_algorithm=cma_maega \
+                                --sigma0=1.0 \
+                                --restart_rule=no_improvement \
+                                --calc_gradient_iters=10 \
+                                --move_mean_iters=10 \
+                                --archive_lr=0.5 \
+                                --threshold_min=200 \
+                                --grid_size=$GRID_SIZE \
+                                --logdir=./experiments/paper_ppga_"$ENV_NAME"_adaptive_stddev
