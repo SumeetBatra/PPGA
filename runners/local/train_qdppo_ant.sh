1c2a584543e1c6bef869657608d5660fb0b2928b
Sumeet Batra <sscifi@gmail.com>
2023-01-24 00:55:08 -0800

    add local runners

--- runners/local/train_qdppo_ant.sh
+++ runners/local/train_qdppo_ant.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 # the following params need to be set manually
 # PROJECT_PATH: path to QDax root on your system
 # ENV_NAME: which environment to run on (see qdax/environments/__init__.py for a list of all runnable envs)
@@ -13,29 +12,29 @@ SEED=1111
 RUN_NAME="paper_qdppo_"$ENV_NAME"_seed_"$SEED"_v2"
 echo $RUN_NAME
 python -m algorithm.train_qdppo --env_name=$ENV_NAME \
-                                --rollout_length=128 \
-                                --use_wandb=True \
-                                --seed=$SEED \
-                                --wandb_group=paper \
-                                --num_dims=4 \
-                                --num_minibatches=8 \
-                                --update_epochs=4 \
-                                --normalize_obs=True \
-                                --normalize_rewards=True \
-                                --wandb_run_name=$RUN_NAME \
-                                --popsize=300 \
-                                --env_batch_size=3000 \
-                                --learning_rate=0.001 \
-                                --vf_coef=2 \
-                                --max_grad_norm=1 \
-                                --torch_deterministic=False \
-                                --total_iterations=2000 \
-                                --dqd_algorithm=cma_maega \
-                                --calc_gradient_iters=10 \
-                                --move_mean_iters=10 \
-                                --archive_lr=0.1 \
-                                --restart_rule=no_improvement \
-                                --sigma0=3.0 \
-                                --threshold_min=-500 \
-                                --grid_size=$GRID_SIZE \
-                                --logdir=./experiments/paper_ppga_"$ENV_NAME"_v2
+                                     --rollout_length=128 \
+                                     --use_wandb=True \
+                                     --seed=$SEED \
+                                     --wandb_group=paper \
+                                     --num_dims=4 \
+                                     --num_minibatches=8 \
+                                     --update_epochs=4 \
+                                     --normalize_obs=True \
+                                     --normalize_rewards=True \
+                                     --wandb_run_name=$RUN_NAME \
+                                     --popsize=300 \
+                                     --env_batch_size=3000 \
+                                     --learning_rate=0.001 \
+                                     --vf_coef=2 \
+                                     --max_grad_norm=1 \
+                                     --torch_deterministic=False \
+                                     --total_iterations=2000 \
+                                     --dqd_algorithm=cma_maega \
+                                     --calc_gradient_iters=10 \
+                                     --move_mean_iters=10 \
+                                     --archive_lr=0.1 \
+                                     --restart_rule=no_improvement \
+                                     --sigma0=3.0 \
+                                     --threshold_min=-500 \
+                                     --grid_size=$GRID_SIZE \
+                                     --logdir=./experiments/paper_ppga_"$ENV_NAME"_v2
--- runners/local/train_walker2d_slurm.sh
+++ runners/local/train_walker2d_slurm.sh
@@ -4,7 +4,7 @@
 # ENV_NAME: which environment to run on (see qdax/environments/__init__.py for a list of all runnable envs)
 # then from the project root dir you can run ./scripts/train_pga_me.sh
 
-ENV_NAME="humanoid"
+ENV_NAME="walker2d"
 GRID_SIZE=50  # number of cells per archive dimension
 SEED=1111
 
@@ -25,18 +25,18 @@ python -m algorithm.train_qdppo --env_name=$ENV_NAME \
                                 --wandb_run_name=$RUN_NAME\
                                 --popsize=300 \
                                 --env_batch_size=3000 \
-                                --learning_rate=0.0003 \
+                                --learning_rate=0.001 \
                                 --vf_coef=2 \
                                 --entropy_coef=0.0 \
                                 --target_kl=0.008 \
                                 --max_grad_norm=1 \
                                 --total_iterations=2000 \
                                 --dqd_algorithm=cma_maega \
-                                --sigma0=0.5 \
+                                --sigma0=1.0 \
                                 --restart_rule=no_improvement \
                                 --calc_gradient_iters=10 \
                                 --move_mean_iters=10 \
-                                --archive_lr=0.1 \
+                                --archive_lr=0.5 \
                                 --threshold_min=200 \
                                 --grid_size=$GRID_SIZE \
                                 --logdir=./experiments/paper_ppga_"$ENV_NAME"_adaptive_stddev
